a91d985eb608d1dbdc1cf290dbfd22b5154cb224
Shubham Mishra <mishrashubham268@gmail.com>
2024-02-27 20:36:54 +0530

    Update Codebase

---
+++ single_dir_conversion.py
@@ -0,0 +1,40 @@
+import os
+import gzip
+import csv
+
+
+# Function to unzip .gz file and convert to CSV
+def unzip_and_convert_to_csv(input_file, output_file):
+    with gzip.open(input_file, "rt") as f_in:
+        with open(output_file, "w", newline="") as f_out:
+            # Read the content and split it into lines
+            content = f_in.read()
+            lines = content.split("\n")
+
+            # Create a CSV writer
+            csv_writer = csv.writer(f_out)
+
+            # Write each line as a row in the CSV file
+            for line in lines:
+                csv_writer.writerow([line])
+
+
+# Define the path to the directory containing gzipped files
+directory_path = r"folder_path"
+
+
+# Function to process all .gz files in the specified directory
+def process_gz_files_in_directory(directory_path):
+    for file in os.listdir(directory_path):
+        if file.endswith(".gz"):
+            gzipped_file_path = os.path.join(directory_path, file)
+            csv_output_file = os.path.join(
+                directory_path, file[:-3] + ".csv"
+            )  # Output CSV file path
+            unzip_and_convert_to_csv(gzipped_file_path, csv_output_file)
+
+
+# Call the function to process all .gz files in the specified directory
+process_gz_files_in_directory(directory_path)
+
+print("Unzipping and conversion to CSV complete!")
